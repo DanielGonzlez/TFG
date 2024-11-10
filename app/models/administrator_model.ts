88c7556a0d27f7db83939987d978969025122af4
DanielGonzlez <frikiporexcelencia@gmail.com>
2024-11-10 01:31:09 +0100

    fix: minor changes

--- app/models/administrator_model.ts
+++ app/models/administrator_model.ts
@@ -1,5 +1,6 @@
 import { DateTime } from 'luxon'
 import { BaseModel, column, hasMany, hasOne } from '@adonisjs/lucid/orm'
+
 import type{ HasOne } from '@adonisjs/lucid/types/relations'
 import type{ HasMany } from '@adonisjs/lucid/types/relations'
 
