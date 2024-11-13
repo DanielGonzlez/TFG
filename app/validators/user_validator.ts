f12b8a6bdb070d609f4952bd833f1b195a459bbb
DanielGonzlez <frikiporexcelencia@gmail.com>
2024-11-13 19:17:23 +0100

    feat: added validator por user entries

--- app/validators/user_validator.ts
+++ app/validators/user_validator.ts
@@ -1,37 +1,21 @@
-import vine from '@vinejs/vine';
+import vine from '@vinejs/vine'
 
-export const createUserSchema = vine.object({
-	name: vine.string().trim(),
-	first_name: vine.string().trim(),
-	last_name: vine.string().trim().optional(),
-	email: vine.string().normalizeEmail({
-		all_lowercase: true,
-		gmail_remove_dots: true,
-	}),
-	password: vine.string(),
-}).toCamelCase();
+// Esquema de validación de registro
+export const RegisterSchema = vine.object({
+  name: vine.string().trim().minLength(3).maxLength(50),
+  firstName: vine.string().trim().minLength(3).maxLength(50),
+  lastName: vine.string().trim().minLength(3).maxLength(50).optional(),
+  email: vine.string()
+    .email()  // Validación básica de formato de email
+    .normalizeEmail({
+      all_lowercase: true,
+      gmail_remove_dots: true,
+    }),
+  password: vine.string().minLength(6).maxLength(20),
+  billingAddress: vine.string().minLength(10).maxLength(200),
+  isWholesaler: vine.boolean().optional(),
+  organizationId: vine.string().optional()
+}).toCamelCase()
 
-export const createUserValidator = vine.compile(createUserSchema);
-
-export const updateUserValidator = vine.compile(
-	vine.object({
-		email: vine.string().email()
-	}).toCamelCase()
-);
-
-export const loginUserValidator = vine.compile(
-	vine.object({
-		email: vine.string().trim(),
-		password: vine.string(),
-	}).toCamelCase()
-);
-
-export const statisticsUserValidator = vine.object({
-	from: vine.date({
-		formats: ['D/M/YYYY']
-	}),
-	to: vine.date({
-		formats: ['D/M/YYYY']
-	})
-
-}).toCamelCase();
+// Compilación del validador para su uso
+export const RegisterValidator = vine.compile(RegisterSchema)
