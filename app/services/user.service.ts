09b263e3609cb0d78d8aac7b4b5f969a5636b750
DanielGonzlez <frikiporexcelencia@gmail.com>
2024-11-18 02:52:48 +0100

    added funcion validateUser and showUserInfo

--- app/services/user.service.ts
+++ app/services/user.service.ts
@@ -1,5 +1,5 @@
 import User from '#models/user_model';
-import hash from '@adonisjs/core/services/hash';
+import Hash from '@adonisjs/core/services/hash';
 
 import { USER_ROL } from '#types/user_type';
 import { USER_STATUS } from '#types/user_type';
@@ -10,14 +10,12 @@ export default class UserService {
 
   public async createUser(data: { name: string; firstName: string; lastName: string; email: string; password: string }) {
 
-    const hashedPassword = await hash.make(data.password)
-
     const user = await User.create({
       name: data.name,
       firstName: data.firstName,
       lastName: data.lastName,
       email: data.email,
-      password: hashedPassword,
+      password: data.password,
       status: USER_STATUS.ACTIVE,
       rol: USER_ROL.CLIENT,
     })
@@ -25,4 +23,19 @@ export default class UserService {
     return user
   }
 
+  public async validateUser(data: { email: string; password: string }) {
+    const user = await User.query().where('email', data.email).firstOrFail()
+    const storedPassword = user.password
+    const isValid = await Hash.verify(storedPassword, data.password)
+    if (isValid) {
+      return user
+    }
+    throw new Error('Invalid credentials')
+  }
+
+  //enseñar informacion del usuario sacandola de la base de datos
+  public async showUserInfo(userId: string) {
+    const user = await User.query().where('userId', userId).first()
+    return user
+  }
 }
