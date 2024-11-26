be3beb01a6b4f5175ec6dc35fb5ed65423842425
DanielGonzlez <frikiporexcelencia@gmail.com>
2024-11-26 03:14:30 +0100

    feat: implemented non-functional script

---
+++ resources/js/carga-asincrona.js
@@ -0,0 +1,21 @@
+/*$(document).ready(function() {
+    // Realizar la llamada asíncrona para obtener el contenido
+    $.ajax({
+        url: "{{ route('productos') }}",
+        type: 'GET',
+        success: function(response) {
+            // Mostrar el contenido una vez que se haya cargado
+
+            // IMPLEMENTAR LUEGO 
+
+            // $('#contenedorOfertas').html(response);
+            // $('#contenedorOfertas').fadeIn(); // Mostrar el contenido con un efecto de fundido
+        },
+        error: function(xhr, status, error) {
+            // Manejar el error si la llamada falla
+            console.error(error);
+        }
+    });
+});
+
+*/
