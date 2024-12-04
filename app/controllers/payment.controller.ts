3c1cf99a6ef2b35328a0f700378a0219d73e0ab2
DanielGonzlez <frikiporexcelencia@gmail.com>
2024-12-04 03:13:57 +0100

    feat: controller created

--- app/controllers/payment.controller.ts
+++ app/controllers/payment.controller.ts
@@ -15,19 +15,20 @@ export default class PaymentController {
     const total = cart ? Object.values(cart).reduce((acc:any, item:any) => acc + (parseFloat(item.discountedPrice) * item.quantity), 0) : 0
 
     if (!cart || cart.length === 0) {
-      return response.status(400).json({ error: 'El carrito está vacío.' }) // Maneja el error de carrito vacío
+      return response.status(400).json({ error: 'El carrito está vacío.' }) //* Maneja el error de carrito vacío
     }
 
     try {
-      // Crear un PaymentIntent para simular el pago
+      //! Crear un PaymentIntent para simular el pago
+      //! Si se quisiera hacer un pago real, se debe usar Stripe.confirmCardPayment
       const paymentIntent = await this.stripe.paymentIntents.create({
-        amount: Number(total) * 100, // Monto simulado en centavos (ej. 1000 = 10.00)
-        currency: 'eur', // Moneda
+        amount: Number(total) * 100, //! Monto simulado en centimos que pasaran a euros (ej. 100 = 10.00€)
+        currency: 'eur',
         payment_method_types: ['card'],
       })
 
       return response.json({
-        clientSecret: paymentIntent.client_secret, // Envía el clientSecret para confirmación
+        clientSecret: paymentIntent.client_secret, //* Envía el clientSecret para confirmación
       })
     } catch (error) {
       console.error('Error al crear PaymentIntent:', error)
@@ -35,9 +36,8 @@ export default class PaymentController {
     }
   }
 
-  // Nueva ruta para borrar el carrito después del pago
   public async clearCart({ session, response }: HttpContext) {
-    session.forget('cart') // Elimina el carrito de la sesión
+    session.forget('cart')
     return response.json({ message: 'Carrito eliminado con éxito.' })
   }
 }
